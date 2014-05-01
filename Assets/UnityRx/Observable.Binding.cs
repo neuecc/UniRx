7ef3bbcb70a5ec03c619411d5fb7a246aa5d9b29
neuecc <ils@neue.cc>
2014-05-01 21:33:28 +0900

    add Publish(with behavior), Replay

--- Assets/UnityRx/Observable.Binding.cs
+++ Assets/UnityRx/Observable.Binding.cs
@@ -18,22 +18,50 @@ namespace UnityRx
             return source.Multicast(new Subject<T>());
         }
 
-        // not yet
-        //public static IConnectableObservable<T> Publish<T>(this IObservable<T> source, T initialValue)
-        //{
-        //    return source.Multicast(new BehaviourSubject<T>(initialValue));
-        //}
+        public static IConnectableObservable<T> Publish<T>(this IObservable<T> source, T initialValue)
+        {
+            return source.Multicast(new BehaviorSubject<T>(initialValue));
+        }
 
         public static IConnectableObservable<T> PublishLast<T>(this IObservable<T> source)
         {
             return source.Multicast(new AsyncSubject<T>());
         }
 
-        // not yet
-        //public static IConnectableObservable<T> Replay<T>(this IObservable<T> source)
-        //{
-        //    return source.Multicast(new ReplaySubject<T>());
-        //}
+        public static IConnectableObservable<T> Replay<T>(this IObservable<T> source)
+        {
+            return source.Multicast(new ReplaySubject<T>());
+        }
+
+        public static IConnectableObservable<T> Replay<T>(this IObservable<T> source, IScheduler scheduler)
+        {
+            return source.Multicast(new ReplaySubject<T>(scheduler));
+        }
+
+        public static IConnectableObservable<T> Replay<T>(this IObservable<T> source, int bufferSize)
+        {
+            return source.Multicast(new ReplaySubject<T>(bufferSize));
+        }
+
+        public static IConnectableObservable<T> Replay<T>(this IObservable<T> source, int bufferSize, IScheduler scheduler)
+        {
+            return source.Multicast(new ReplaySubject<T>(bufferSize, scheduler));
+        }
+
+        public static IConnectableObservable<T> Replay<T>(this IObservable<T> source, TimeSpan window)
+        {
+            return source.Multicast(new ReplaySubject<T>(window));
+        }
+
+        public static IConnectableObservable<T> Replay<T>(this IObservable<T> source, TimeSpan window, IScheduler scheduler)
+        {
+            return source.Multicast(new ReplaySubject<T>(window, scheduler));
+        }
+
+        public static IConnectableObservable<T> Replay<T>(this IObservable<T> source, int bufferSize, TimeSpan window, IScheduler scheduler)
+        {
+            return source.Multicast(new ReplaySubject<T>(bufferSize, window, scheduler));
+        }
 
         public static IObservable<T> RefCount<T>(this IConnectableObservable<T> source)
         {
