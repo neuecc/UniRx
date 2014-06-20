6f9ce2e0ac17f5fbaa62e1f57b2f49b31b2d9b9e
neuecc <ils@neue.cc>
2014-06-21 03:50:37 +0900

    FromCoroutine improved suports for observer less IEnumerator

--- Assets/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
+++ Assets/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
@@ -3,11 +3,85 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace UniRx
 {
     public static partial class Observable
     {
+        /// <summary>From has no callback coroutine to IObservable. If publishEveryYield = true then publish OnNext every yield return else return once on enumeration completed.</summary>
+        public static IObservable<Unit> FromCoroutine(Func<IEnumerator> coroutine, bool publishEveryYield = false)
+        {
+            return FromCoroutine<Unit>((observer, cancellationToken) => WrapEnumerator(coroutine(), observer, cancellationToken, publishEveryYield));
+        }
+
+        static IEnumerator WrapEnumerator(IEnumerator enumerator, IObserver<Unit> observer, CancellationToken cancellationToken, bool publishEveryYield)
+        {
+            var hasNext = default(bool);
+            var raisedError = false;
+            do
+            {
+                try
+                {
+                    hasNext = enumerator.MoveNext();
+                }
+                catch (Exception ex)
+                {
+                    try
+                    {
+                        raisedError = true;
+                        observer.OnError(ex);
+                    }
+                    finally
+                    {
+                        var d = enumerator as IDisposable;
+                        if (d != null)
+                        {
+                            d.Dispose();
+                        }
+                    }
+                    yield break;
+                }
+                if (hasNext && publishEveryYield)
+                {
+                    try
+                    {
+                        observer.OnNext(Unit.Default);
+                    }
+                    catch
+                    {
+                        var d = enumerator as IDisposable;
+                        if (d != null)
+                        {
+                            d.Dispose();
+                        }
+                        throw;
+                    }
+                }
+                if (hasNext)
+                {
+                    yield return enumerator.Current; // yield inner YieldInstruction
+                }
+            } while (hasNext && !cancellationToken.IsCancellationRequested);
+
+            try
+            {
+                if (!raisedError && !cancellationToken.IsCancellationRequested)
+                {
+                    observer.OnNext(Unit.Default); // last one
+                    observer.OnCompleted();
+                }
+            }
+            finally
+            {
+                var d = enumerator as IDisposable;
+                if (d != null)
+                {
+                    d.Dispose();
+                }
+            }
+        }
+
         public static IObservable<T> FromCoroutine<T>(Func<IObserver<T>, IEnumerator> coroutine)
         {
             return FromCoroutine<T>((observer, _) => coroutine(observer));
@@ -24,6 +98,23 @@ namespace UniRx
                 return cancel;
             });
         }
+
+        public static IObservable<Unit> SelectMany<T>(this IObservable<T> source, IEnumerator coroutine, bool publishEveryYield = false)
+        {
+            return source.SelectMany(Observable.FromCoroutine(() => coroutine, publishEveryYield));
+        }
+
+        public static IObservable<Unit> SelectMany<T>(this IObservable<T> source, Func<T, IEnumerator> selector, bool publishEveryYield = false)
+        {
+            return source.SelectMany(x => Observable.FromCoroutine(() => selector(x), publishEveryYield));
+        }
+
+        public static IObservable<Unit> ToObservable(this IEnumerator coroutine, bool publishEveryYield = false)
+        {
+            return FromCoroutine<Unit>((observer, cancellationToken) => WrapEnumerator(coroutine, observer, cancellationToken, publishEveryYield));
+        }
+
+        // variation of FromCoroutine
 
         public static IObservable<long> EveryUpdate()
         {
