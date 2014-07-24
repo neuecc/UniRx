f11f7dff3d374300f6d3690f7230a4b2741bb0a4
neuecc <ils@neue.cc>
2014-07-24 22:27:43 +0900

    Add IDisposable.AddTo Extension Methods #11

---
+++ Assets/UniRx/Scripts/Disposables/DisposableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniRx
+{
+    public static class DisposableExtensions
+    {
+        /// <summary>Add disposable(self) to CompositeDisposable(or other ICollection)</summary>
+        public static void AddTo(this IDisposable disposable, ICollection<IDisposable> container)
+        {
+            container.Add(disposable);
+        }
+    }
+}
