325a8430277701350c4c04548eabaab129e03396
neuecc <ils@neue.cc>
2014-08-05 19:01:48 +0900

    add DefaultSchedulers #25

--- Assets/UniRx/Scripts/Schedulers/Scheduler.cs
+++ Assets/UniRx/Scripts/Schedulers/Scheduler.cs
@@ -20,6 +20,93 @@ namespace UniRx
     // Scheduler Extension
     public static partial class Scheduler
     {
+        // configurable defaults
+        public static class DefaultSchedulers
+        {
+            static IScheduler constantTime;
+            public static IScheduler ConstantTimeOperations
+            {
+                get
+                {
+                    return constantTime ?? (constantTime = Scheduler.Immediate);
+                }
+                set
+                {
+                    constantTime = value;
+                }
+            }
+
+            static IScheduler tailRecursion;
+            public static IScheduler TailRecursion
+            {
+                get
+                {
+                    return tailRecursion ?? (tailRecursion = Scheduler.Immediate);
+                }
+                set
+                {
+                    tailRecursion = value;
+                }
+            }
+
+            static IScheduler iteration;
+            public static IScheduler Iteration
+            {
+                get
+                {
+                    return iteration ?? (iteration = Scheduler.CurrentThread);
+                }
+                set
+                {
+                    iteration = value;
+                }
+            }
+
+            static IScheduler timeBasedOperations;
+            public static IScheduler TimeBasedOperations
+            {
+                get
+                {
+                    return timeBasedOperations ?? (timeBasedOperations = Scheduler.MainThread); // MainThread as default for TimeBased Operation
+                }
+                set
+                {
+                    timeBasedOperations = value;
+                }
+            }
+
+            static IScheduler asyncConversions;
+            public static IScheduler AsyncConversions
+            {
+                get
+                {
+                    return asyncConversions ?? (asyncConversions = Scheduler.ThreadPool);
+                }
+                set
+                {
+                    asyncConversions = value;
+                }
+            }
+
+            public static void SetDefaultForUnity()
+            {
+                ConstantTimeOperations = Scheduler.Immediate;
+                TailRecursion = Scheduler.Immediate;
+                Iteration = Scheduler.CurrentThread;
+                TimeBasedOperations = Scheduler.MainThread;
+                AsyncConversions = Scheduler.ThreadPool;
+            }
+
+            public static void SetDotNetCompatible()
+            {
+                ConstantTimeOperations = Scheduler.Immediate;
+                TailRecursion = Scheduler.Immediate;
+                Iteration = Scheduler.CurrentThread;
+                TimeBasedOperations = Scheduler.ThreadPool;
+                AsyncConversions = Scheduler.ThreadPool;
+            }
+        }
+
         // utils
 
         public static DateTimeOffset Now
