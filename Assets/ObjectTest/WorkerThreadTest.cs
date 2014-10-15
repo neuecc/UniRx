71650f15b8d08831c35e0454cc3217df90c85568
movrajr <movrajr@gmail.com>
2014-10-15 22:36:48 +0200

    Added scene explaining multiple MainThreadDispatchers handling

--- Assets/ObjectTest/WorkerThreadTest.cs
+++ Assets/ObjectTest/WorkerThreadTest.cs
@@ -3,25 +3,28 @@ using System.Threading;
 using UniRx;
 using UnityEngine;
 
-public class WorkerThreadTest : MonoBehaviour
+namespace UniRx.ObjectTest
 {
-    void Awake()
+    public class WorkerThreadTest : MonoBehaviour
     {
-        Thread worker = new Thread(() =>
+        void Awake()
         {
-            // Bind to application quit event.
-            MainThreadDispatcher.OnApplicationQuitAsObservable().Subscribe(_ => Debug.Log("OnApplicationQuitAsObservable"));
+            Thread worker = new Thread(() =>
+            {
+                // Bind to application quit event.
+                MainThreadDispatcher.OnApplicationQuitAsObservable().Subscribe(_ => Debug.Log("OnApplicationQuitAsObservable"));
 
-            // Create capsules one by one.
-            Observable.Interval(TimeSpan.FromMilliseconds(300))
-                .Take(5)
-                .Subscribe((s) =>
-                {
-                    var g = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                    g.name = "Capsule " + s;
-                    g.transform.position += new Vector3(s, 0, 0);
-                });
-        });
-        worker.Start();
+                // Create capsules one by one.
+                Observable.Interval(TimeSpan.FromMilliseconds(300))
+                    .Take(5)
+                    .Subscribe((s) =>
+                    {
+                        var g = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                        g.name = "Capsule " + s;
+                        g.transform.position += new Vector3(s, 0, 0);
+                    });
+            });
+            worker.Start();
+        }
     }
 }
