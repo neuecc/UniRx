4de7b47f3362d38995da5b9e38bb3200adcfe278
movrajr <movrajr@gmail.com>
2014-11-08 12:42:49 +0100

    Replaced Thread with ThreadPool for Windows Store platform

--- Assets/ObjectTest/WorkerThreadTest.cs
+++ Assets/ObjectTest/WorkerThreadTest.cs
@@ -9,21 +9,34 @@ namespace UniRx.ObjectTest
     {
         void Awake()
         {
-            ThreadPool.QueueUserWorkItem((workItem) =>
-            {
-                // Bind to application quit event.
-                MainThreadDispatcher.OnApplicationQuitAsObservable().Subscribe(_ => Debug.Log("OnApplicationQuitAsObservable"));
+            ThreadPool.QueueUserWorkItem(RegisterApplicationQuitEvent);
 
-                // Create capsules one by one.
-                Observable.Interval(TimeSpan.FromMilliseconds(300))
-                    .Take(5)
-                    .Subscribe((s) =>
-                    {
-                        var g = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                        g.name = "Capsule " + s;
-                        g.transform.position += new Vector3(s, 0, 0);
-                    });
-            });
+#if UNITY_METRO
+            // Windows Store doesn't support System.Threading.Thread.
+            // Other platforms can use both ThreadPool and System.Threading.Thread.
+            ThreadPool.QueueUserWorkItem(SpawnCapsules);
+#else
+            Thread thread = new Thread(SpawnCapsules);
+            thread.Start();
+#endif
+        }
+
+        private void RegisterApplicationQuitEvent(object a)
+        {
+            MainThreadDispatcher.OnApplicationQuitAsObservable().Subscribe(_ => Debug.Log("OnApplicationQuitAsObservable"));
+        }
+
+        private void SpawnCapsules(object a)
+        {
+            // Create capsules one by one.
+            Observable.Interval(TimeSpan.FromMilliseconds(300))
+                .Take(5)
+                .Subscribe((s) =>
+                {
+                    var g = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                    g.name = "Capsule " + s;
+                    g.transform.position += new Vector3(s, 0, 0);
+                });
         }
     }
 }
