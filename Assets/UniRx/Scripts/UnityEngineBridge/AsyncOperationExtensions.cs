ed681ac6348a871474519e380090706dd9ab879a
neuecc <ils@neue.cc>
2014-12-26 12:39:50 +0900

    Add AsyncOperationExtensions.AsObservable

---
+++ Assets/UniRx/Scripts/UnityEngineBridge/AsyncOperationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace UniRx
+{
+    public static partial class AsyncOperationExtensions
+    {
+        public static IObservable<AsyncOperation> AsObservable(this AsyncOperation asyncOperation, IProgress<float> progress = null)
+        {
+            return Observable.FromCoroutine<AsyncOperation>((observer, cancellation) => AsObservableCore(asyncOperation, observer, progress, cancellation));
+        }
+
+        static IEnumerator AsObservableCore(AsyncOperation asyncOperation, IObserver<AsyncOperation> observer, IProgress<float> reportProgress, CancellationToken cancel)
+        {
+            while (!asyncOperation.isDone && !cancel.IsCancellationRequested)
+            {
+                if (reportProgress != null)
+                {
+                    try
+                    {
+                        reportProgress.Report(asyncOperation.progress);
+                    }
+                    catch (Exception ex)
+                    {
+                        observer.OnError(ex);
+                        yield break;
+                    }
+                }
+                yield return null;
+            }
+
+            if (cancel.IsCancellationRequested) yield break;
+
+            observer.OnNext(asyncOperation);
+            observer.OnCompleted();
+        }
+    }
+}
