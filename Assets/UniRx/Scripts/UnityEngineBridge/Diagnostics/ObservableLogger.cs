b355b53377c91e180c1650a73f1895acb4199755
neuecc <ils@neue.cc>
2015-01-21 23:35:13 +0900

    ObservableLogger doesn't manage logger name(it's bad idea)

--- Assets/UniRx/Scripts/UnityEngineBridge/Diagnostics/ObservableLogger.cs
+++ Assets/UniRx/Scripts/UnityEngineBridge/Diagnostics/ObservableLogger.cs
@@ -7,7 +7,6 @@ namespace UniRx.Diagnostics
 {
     public class ObservableLogger : IObservable<LogEntry>
     {
-        static readonly Dictionary<string, Logger> loggerList = new Dictionary<string, Logger>();
         static readonly Subject<LogEntry> logPublisher = new Subject<LogEntry>();
 
         public static readonly ObservableLogger Listener = new ObservableLogger();
@@ -19,13 +18,7 @@ namespace UniRx.Diagnostics
 
         public static Action<LogEntry> RegisterLogger(Logger logger)
         {
-            lock (loggerList)
-            {
-                if (logger.Name == null) throw new ArgumentNullException("logger.Name is null");
-                if (loggerList.ContainsKey(logger.Name)) throw new ArgumentException("Duplicate Key" + logger.Name);
-
-                loggerList.Add(logger.Name, logger);
-            }
+            if (logger.Name == null) throw new ArgumentNullException("logger.Name is null");
 
             return logPublisher.OnNext;
         }
