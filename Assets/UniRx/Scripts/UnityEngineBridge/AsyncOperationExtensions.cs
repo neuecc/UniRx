39c4c07d9797c539097366fd9d97f62382324329
neuecc <ils@neue.cc>
2015-02-24 00:41:42 +0900

    Add AsyncOperation.AsAsyncOperationObservable

--- Assets/UniRx/Scripts/UnityEngineBridge/AsyncOperationExtensions.cs
+++ Assets/UniRx/Scripts/UnityEngineBridge/AsyncOperationExtensions.cs
@@ -6,12 +6,23 @@ namespace UniRx
 {
     public static partial class AsyncOperationExtensions
     {
+        /// <summary>
+        /// If you needs return value, use AsAsyncOperationObservable instead.
+        /// </summary>
         public static IObservable<AsyncOperation> AsObservable(this AsyncOperation asyncOperation, IProgress<float> progress = null)
         {
             return Observable.FromCoroutine<AsyncOperation>((observer, cancellation) => AsObservableCore(asyncOperation, observer, progress, cancellation));
         }
 
-        static IEnumerator AsObservableCore(AsyncOperation asyncOperation, IObserver<AsyncOperation> observer, IProgress<float> reportProgress, CancellationToken cancel)
+        // T: where T : AsyncOperation is ambigious with IObservable<T>.AsObservable
+        public static IObservable<T> AsAsyncOperationObservable<T>(this T asyncOperation, IProgress<float> progress = null)
+            where T : AsyncOperation
+        {
+            return Observable.FromCoroutine<T>((observer, cancellation) => AsObservableCore(asyncOperation, observer, progress, cancellation));
+        }
+
+        static IEnumerator AsObservableCore<T>(T asyncOperation, IObserver<T> observer, IProgress<float> reportProgress, CancellationToken cancel)
+            where T : AsyncOperation
         {
             while (!asyncOperation.isDone && !cancel.IsCancellationRequested)
             {
