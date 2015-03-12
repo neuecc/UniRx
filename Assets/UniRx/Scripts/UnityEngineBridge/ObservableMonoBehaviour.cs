0dce934993179de786e88f14c0c15a741ae0b389
neuecc <ils@neue.cc>
2015-03-12 21:22:11 +0900

    ObservableMonoBehaviour.Awake, Start, Destroy invoke OnCompleted

--- Assets/UniRx/Scripts/UnityEngineBridge/ObservableMonoBehaviour.cs
+++ Assets/UniRx/Scripts/UnityEngineBridge/ObservableMonoBehaviour.cs
@@ -5,17 +5,20 @@ namespace UniRx
 {
     public class ObservableMonoBehaviour : TypedMonoBehaviour
     {
+        bool calledAwake = false;
         Subject<Unit> awake;
 
         /// <summary>Awake is called when the script instance is being loaded.</summary>
         public override void Awake()
         {
-            if (awake != null) awake.OnNext(Unit.Default);
+            calledAwake = true;
+            if (awake != null) { awake.OnNext(Unit.Default); awake.OnCompleted(); }
         }
 
         /// <summary>Awake is called when the script instance is being loaded.</summary>
         public IObservable<Unit> AwakeAsObservable()
         {
+            if (calledAwake) return Observable.Return(Unit.Default);
             return awake ?? (awake = new Subject<Unit>());
         }
 
@@ -271,17 +274,20 @@ namespace UniRx
             return onControllerColliderHit ?? (onControllerColliderHit = new Subject<ControllerColliderHit>());
         }
 
+        bool calledDestroy = false;
         Subject<Unit> onDestroy;
 
         /// <summary>This function is called when the MonoBehaviour will be destroyed.</summary>
         public override void OnDestroy()
         {
-            if (onDestroy != null) onDestroy.OnNext(Unit.Default);
+            calledDestroy = true;
+            if (onDestroy != null) { onDestroy.OnNext(Unit.Default); onDestroy.OnCompleted(); }
         }
 
         /// <summary>This function is called when the MonoBehaviour will be destroyed.</summary>
         public IObservable<Unit> OnDestroyAsObservable()
         {
+            if (calledDestroy) return Observable.Return(Unit.Default);
             return onDestroy ?? (onDestroy = new Subject<Unit>());
         }
 
@@ -713,17 +719,20 @@ namespace UniRx
             return reset ?? (reset = new Subject<Unit>());
         }
 
+        bool calledStart = false;
         Subject<Unit> start;
 
         /// <summary>Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.</summary>
         public override void Start()
         {
-            if (start != null) start.OnNext(Unit.Default);
+            calledStart = true;
+            if (start != null) { start.OnNext(Unit.Default); start.OnCompleted(); }
         }
 
         /// <summary>Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.</summary>
         public IObservable<Unit> StartAsObservable()
         {
+            if (calledStart) return Observable.Return(Unit.Default);
             return start ?? (start = new Subject<Unit>());
         }
 
