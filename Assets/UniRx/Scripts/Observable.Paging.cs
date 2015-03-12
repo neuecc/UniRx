68a3708a3dc672c44bd0985aec13f10562a01e8d
neuecc <ils@neue.cc>
2015-03-12 21:04:45 +0900

    Add Observable.Pairwise

--- Assets/UniRx/Scripts/Observable.Paging.cs
+++ Assets/UniRx/Scripts/Observable.Paging.cs
@@ -561,6 +561,42 @@ namespace UniRx
             });
         }
 
+        /// <summary>Projects old and new element of a sequence into a new form.</summary>
+        public static IObservable<TR> Pairwise<T, TR>(this IObservable<T> source, Func<T, T, TR> selector)
+        {
+            var result = Observable.Create<TR>(observer =>
+            {
+                T prev = default(T);
+                var isFirst = true;
+
+                return source.Subscribe(x =>
+                {
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                        prev = x;
+                        return;
+                    }
+
+                    TR value;
+                    try
+                    {
+                        value = selector(prev, x);
+                        prev = x;
+                    }
+                    catch (Exception ex)
+                    {
+                        observer.OnError(ex);
+                        return;
+                    }
+
+                    observer.OnNext(value);
+                }, observer.OnError, observer.OnCompleted);
+            });
+
+            return result;
+        }
+
         // first, last, single
 
         public static IObservable<T> Last<T>(this IObservable<T> source)
