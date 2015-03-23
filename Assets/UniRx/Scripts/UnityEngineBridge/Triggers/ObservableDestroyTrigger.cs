bc70f8fb2b33c92e27b8354c78fa3807920f30e3
neuecc <ils@neue.cc>
2015-03-23 23:57:52 +0900

    Add ObservableDestroyTrigger

---
+++ Assets/UniRx/Scripts/UnityEngineBridge/Triggers/ObservableDestroyTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace UniRx.Triggers
+{
+    public class ObservableDestroyTrigger : MonoBehaviour
+    {
+        bool calledDestroy = false;
+        Subject<Unit> onDestroy;
+
+        /// <summary>This function is called when the MonoBehaviour will be destroyed.</summary>
+        void OnDestroy()
+        {
+            calledDestroy = true;
+            if (onDestroy != null) { onDestroy.OnNext(Unit.Default); onDestroy.OnCompleted(); }
+        }
+
+        /// <summary>This function is called when the MonoBehaviour will be destroyed.</summary>
+        public IObservable<Unit> OnDestroyAsObservable()
+        {
+            if (calledDestroy) return Observable.Return(Unit.Default);
+            return onDestroy ?? (onDestroy = new Subject<Unit>());
+        }
+    }
+}
