bda3c426bf14aff87484789d6a76752dc899af10
neuecc <ils@neue.cc>
2015-03-31 18:33:14 +0900

    ObservableTriggerExtensions complete

--- Assets/UniRx/Scripts/UnityEngineBridge/Triggers/ObservableTriggerExtensions.cs
+++ Assets/UniRx/Scripts/UnityEngineBridge/Triggers/ObservableTriggerExtensions.cs
@@ -1,19 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace UniRx.Triggers
 {
     public static partial class ObservableTriggerExtensions
     {
-        #region ObservableUpdateTrigger
+        #region ObservableAnimatorTrigger
 
-        /// <summary>Update is called every frame, if the MonoBehaviour is enabled.</summary>
-        public static IObservable<Unit> UpdateAsObservable(this GameObject gameObject)
+        /// <summary>Callback for setting up animation IK (inverse kinematics).</summary>
+        public static IObservable<int> OnAnimatorIKAsObservable(this GameObject gameObject)
         {
-            return GetOrAddComponent<ObservableUpdateTrigger>(gameObject).UpdateAsObservable();
+            return GetOrAddComponent<ObservableAnimatorTrigger>(gameObject).OnAnimatorIKAsObservable();
+        }
+
+        /// <summary>Callback for processing animation movements for modifying root motion.</summary>
+        public static IObservable<Unit> OnAnimatorMoveAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableAnimatorTrigger>(gameObject).OnAnimatorMoveAsObservable();
+        }
+
+        #endregion
+
+        #region ObservableCollision2DTrigger
+
+        /// <summary>Sent when an incoming collider makes contact with this object's collider (2D physics only).</summary>
+        public static IObservable<Collision2D> OnCollisionEnter2DAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableCollision2DTrigger>(gameObject).OnCollisionEnter2DAsObservable();
+        }
+
+
+        /// <summary>Sent when a collider on another object stops touching this object's collider (2D physics only).</summary>
+        public static IObservable<Collision2D> OnCollisionExit2DAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableCollision2DTrigger>(gameObject).OnCollisionExit2DAsObservable();
+        }
+
+        /// <summary>Sent each frame where a collider on another object is touching this object's collider (2D physics only).</summary>
+        public static IObservable<Collision2D> OnCollisionStay2DAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableCollision2DTrigger>(gameObject).OnCollisionStay2DAsObservable();
+        }
+
+        #endregion
+
+        #region ObservableCollisionTrigger
+
+        /// <summary>OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider.</summary>
+        public static IObservable<Collision> OnCollisionEnterAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableCollisionTrigger>(gameObject).OnCollisionEnterAsObservable();
+        }
+
+
+        /// <summary>OnCollisionExit is called when this collider/rigidbody has stopped touching another rigidbody/collider.</summary>
+        public static IObservable<Collision> OnCollisionExitAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableCollisionTrigger>(gameObject).OnCollisionExitAsObservable();
+        }
+
+        /// <summary>OnCollisionStay is called once per frame for every collider/rigidbody that is touching rigidbody/collider.</summary>
+        public static IObservable<Collision> OnCollisionStayAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableCollisionTrigger>(gameObject).OnCollisionStayAsObservable();
+        }
+
+        #endregion
+
+        #region ObservableDestroyTrigger
+
+        /// <summary>This function is called when the MonoBehaviour will be destroyed.</summary>
+        public static IObservable<Unit> OnDestroyAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableDestroyTrigger>(gameObject).OnDestroyAsObservable();
+        }
+
+        #endregion
+
+
+        #region ObservableEnableTrigger
+
+        /// <summary>This function is called when the object becomes enabled and active.</summary>
+        public static IObservable<Unit> OnEnableAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableEnableTrigger>(gameObject).OnEnableAsObservable();
+        }
+
+        /// <summary>This function is called when the behaviour becomes disabled () or inactive.</summary>
+        public static IObservable<Unit> OnDisableAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableEnableTrigger>(gameObject).OnDisableAsObservable();
         }
 
         #endregion
@@ -38,44 +113,124 @@ namespace UniRx.Triggers
 
         #endregion
 
-        #region ObservableDestroyTrigger
+        #region ObservableMouseTrigger
 
-        /// <summary>This function is called when the MonoBehaviour will be destroyed.</summary>
-        public static IObservable<Unit> OnDestroyAsObservable(this GameObject gameObject)
+#if !(UNITY_IPHONE || UNITY_ANDROID)
+
+        /// <summary>OnMouseDown is called when the user has pressed the mouse button while over the GUIElement or Collider.</summary>
+        public static IObservable<Unit> OnMouseDownAsObservable(this GameObject gameObject)
         {
-            return GetOrAddComponent<ObservableDestroyTrigger>(gameObject).OnDestroyAsObservable();
+            return GetOrAddComponent<ObservableMouseTrigger>(gameObject).OnMouseDownAsObservable();
+        }
+
+        /// <summary>OnMouseDrag is called when the user has clicked on a GUIElement or Collider and is still holding down the mouse.</summary>
+        public static IObservable<Unit> OnMouseDragAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableMouseTrigger>(gameObject).OnMouseDragAsObservable();
+        }
+
+        /// <summary>OnMouseEnter is called when the mouse entered the GUIElement or Collider.</summary>
+        public static IObservable<Unit> OnMouseEnterAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableMouseTrigger>(gameObject).OnMouseEnterAsObservable();
+        }
+
+        /// <summary>OnMouseExit is called when the mouse is not any longer over the GUIElement or Collider.</summary>
+        public static IObservable<Unit> OnMouseExitAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableMouseTrigger>(gameObject).OnMouseExitAsObservable();
+        }
+
+        /// <summary>OnMouseOver is called every frame while the mouse is over the GUIElement or Collider.</summary>
+        public static IObservable<Unit> OnMouseOverAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableMouseTrigger>(gameObject).OnMouseOverAsObservable();
+        }
+
+        /// <summary>OnMouseUp is called when the user has released the mouse button.</summary>
+        public static IObservable<Unit> OnMouseUpAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableMouseTrigger>(gameObject).OnMouseUpAsObservable();
+        }
+
+        /// <summary>OnMouseUpAsButton is only called when the mouse is released over the same GUIElement or Collider as it was pressed.</summary>
+        public static IObservable<Unit> OnMouseUpAsButtonAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableMouseTrigger>(gameObject).OnMouseUpAsButtonAsObservable();
+        }
+
+#endif
+
+        #endregion
+
+        #region ObservableTrigger2DTrigger
+
+        /// <summary>Sent when another object enters a trigger collider attached to this object (2D physics only).</summary>
+        public static IObservable<Collider2D> OnTriggerEnter2DAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableTrigger2DTrigger>(gameObject).OnTriggerEnter2DAsObservable();
+        }
+
+
+        /// <summary>Sent when another object leaves a trigger collider attached to this object (2D physics only).</summary>
+        public static IObservable<Collider2D> OnTriggerExit2DAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableTrigger2DTrigger>(gameObject).OnTriggerExit2DAsObservable();
+        }
+
+        /// <summary>Sent each frame where another object is within a trigger collider attached to this object (2D physics only).</summary>
+        public static IObservable<Collider2D> OnTriggerStay2DAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableTrigger2DTrigger>(gameObject).OnTriggerStay2DAsObservable();
         }
 
         #endregion
 
-        #region ObservableAnimatorTrigger
+        #region ObservableTriggerTrigger
 
-        /// <summary>Callback for setting up animation IK (inverse kinematics).</summary>
-        public static IObservable<int> OnAnimatorIKAsObservable(this GameObject gameObject)
+        /// <summary>OnTriggerEnter is called when the Collider other enters the trigger.</summary>
+        public static IObservable<Collider> OnTriggerEnterAsObservable(this GameObject gameObject)
         {
-            return GetOrAddComponent<ObservableAnimatorTrigger>(gameObject).OnAnimatorIKAsObservable();
+            return GetOrAddComponent<ObservableTriggerTrigger>(gameObject).OnTriggerEnterAsObservable();
         }
 
-        /// <summary>Callback for processing animation movements for modifying root motion.</summary>
-        public static IObservable<Unit> OnAnimatorMoveAsObservable(this GameObject gameObject)
+
+        /// <summary>OnTriggerExit is called when the Collider other has stopped touching the trigger.</summary>
+        public static IObservable<Collider> OnTriggerExitAsObservable(this GameObject gameObject)
         {
-            return GetOrAddComponent<ObservableAnimatorTrigger>(gameObject).OnAnimatorMoveAsObservable();
+            return GetOrAddComponent<ObservableTriggerTrigger>(gameObject).OnTriggerExitAsObservable();
+        }
+
+        /// <summary>OnTriggerStay is called once per frame for every Collider other that is touching the trigger.</summary>
+        public static IObservable<Collider> OnTriggerStayAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableTriggerTrigger>(gameObject).OnTriggerStayAsObservable();
         }
 
         #endregion
 
-        #region ObservableEnableTrigger
+        #region ObservableUpdateTrigger
 
-        /// <summary>This function is called when the object becomes enabled and active.</summary>
-        public static IObservable<Unit> OnEnableAsObservable(this GameObject gameObject)
+        /// <summary>Update is called every frame, if the MonoBehaviour is enabled.</summary>
+        public static IObservable<Unit> UpdateAsObservable(this GameObject gameObject)
         {
-            return GetOrAddComponent<ObservableEnableTrigger>(gameObject).OnEnableAsObservable();
+            return GetOrAddComponent<ObservableUpdateTrigger>(gameObject).UpdateAsObservable();
         }
 
-        /// <summary>This function is called when the behaviour becomes disabled () or inactive.</summary>
-        public static IObservable<Unit> OnDisableAsObservable(this GameObject gameObject)
+        #endregion
+
+        #region ObservableVisibleTrigger
+
+        /// <summary>OnBecameInvisible is called when the renderer is no longer visible by any camera.</summary>
+        public static IObservable<Unit> OnBecameInvisibleAsObservable(this GameObject gameObject)
         {
-            return GetOrAddComponent<ObservableEnableTrigger>(gameObject).OnDisableAsObservable();
+            return GetOrAddComponent<ObservableVisibleTrigger>(gameObject).OnBecameInvisibleAsObservable();
+        }
+
+        /// <summary>OnBecameVisible is called when the renderer became visible by any camera.</summary>
+        public static IObservable<Unit> OnBecameVisibleAsObservable(this GameObject gameObject)
+        {
+            return GetOrAddComponent<ObservableVisibleTrigger>(gameObject).OnBecameVisibleAsObservable();
         }
 
         #endregion
