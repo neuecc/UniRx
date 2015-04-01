4dfe790d259acf33c665460e88151838f0c5c18b
neuecc <ils@neue.cc>
2015-04-01 16:52:42 +0900

    Fix  : UnityEvent.AsObservable makes AOT safe

--- Assets/UniRx/Scripts/UnityEngineBridge/UnityEventExtensions.cs
+++ Assets/UniRx/Scripts/UnityEngineBridge/UnityEventExtensions.cs
@@ -12,33 +12,61 @@ namespace UniRx
     {
         public static IObservable<Unit> AsObservable(this UnityEngine.Events.UnityEvent unityEvent)
         {
-            return Observable.FromEvent<UnityAction>(h => h.Invoke, h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
+            var dummy = 0;
+            return Observable.FromEvent<UnityAction>(h =>
+            {
+                dummy.GetHashCode(); // capture for AOT issue
+                return new UnityAction(h);
+            }, h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
         }
 
         public static IObservable<T> AsObservable<T>(this UnityEngine.Events.UnityEvent<T> unityEvent)
         {
-            return Observable.FromEvent<UnityAction<T>, T>(h => h.Invoke, h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
+            var dummy = 0;
+            return Observable.FromEvent<UnityAction<T>, T>(h =>
+            {
+                dummy.GetHashCode(); // capture for AOT issue
+                return new UnityAction<T>(h);
+            }, h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
         }
 
         public static IObservable<Tuple<T0, T1>> AsObservable<T0, T1>(this UnityEngine.Events.UnityEvent<T0, T1> unityEvent)
         {
-            return Observable.FromEvent<UnityAction<T0, T1>, Tuple<T0, T1>>(
-                h => (t0, t1) => h(Tuple.Create(t0, t1)),
-                h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
+            var dummy = 0;
+            return Observable.FromEvent<UnityAction<T0, T1>, Tuple<T0, T1>>(h =>
+            {
+                return new UnityAction<T0, T1>((t0, t1) =>
+                {
+                    dummy.GetHashCode(); // capture for AOT issue
+                    h(Tuple.Create(t0, t1));
+                });
+            }, h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
         }
 
         public static IObservable<Tuple<T0, T1, T2>> AsObservable<T0, T1, T2>(this UnityEngine.Events.UnityEvent<T0, T1, T2> unityEvent)
         {
-            return Observable.FromEvent<UnityAction<T0, T1, T2>, Tuple<T0, T1, T2>>(
-                h => (t0, t1, t2) => h(Tuple.Create(t0, t1, t2)),
-                h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
+            var dummy = 0;
+            return Observable.FromEvent<UnityAction<T0, T1, T2>, Tuple<T0, T1, T2>>(h =>
+            {
+                return new UnityAction<T0, T1, T2>((t0, t1, t2) =>
+                {
+                    dummy.GetHashCode(); // capture for AOT issue
+                    h(Tuple.Create(t0, t1, t2));
+                });
+            }, h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
         }
 
         public static IObservable<Tuple<T0, T1, T2, T3>> AsObservable<T0, T1, T2, T3>(this UnityEngine.Events.UnityEvent<T0, T1, T2, T3> unityEvent)
         {
-            return Observable.FromEvent<UnityAction<T0, T1, T2, T3>, Tuple<T0, T1, T2, T3>>(
-                h => (t0, t1, t2, t3) => h(Tuple.Create(t0, t1, t2, t3)),
-                h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
+            var dummy = 0;
+            return Observable.FromEvent<UnityAction<T0, T1, T2, T3>, Tuple<T0, T1, T2, T3>>(h =>
+            {
+                return new UnityAction<T0, T1, T2, T3>((t0, t1, t2, t3) =>
+                {
+                    dummy.GetHashCode(); // capture for AOT issue
+                    h(Tuple.Create(t0, t1, t2, t3));
+                });
+            }, h => unityEvent.AddListener(h), h => unityEvent.RemoveListener(h));
         }
     }
 }
