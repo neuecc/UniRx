2c149f99b6887a271d550bf0eeff3619af7bb913
neuecc <ils@neue.cc>
2015-04-02 23:47:22 +0900

    Add IDisposable.AddTo(Component)

--- Assets/UniRx/Scripts/UnityEngineBridge/LifetimeDisposableExtensions.cs
+++ Assets/UniRx/Scripts/UnityEngineBridge/LifetimeDisposableExtensions.cs
@@ -10,6 +10,12 @@ namespace UniRx
         /// <summary>Dispose self on target gameObject has been destroyed. Return value is self disposable.</summary>
         public static IDisposable AddTo(this IDisposable disposable, GameObject gameObject)
         {
+            if (gameObject == null)
+            {
+                disposable.Dispose();
+                return disposable;
+            }
+
             var trigger = gameObject.GetComponent<ObservableDestroyTrigger>();
             if (trigger == null)
             {
@@ -20,6 +26,18 @@ namespace UniRx
             return disposable;
         }
 
+        /// <summary>Dispose self on target gameObject has been destroyed. Return value is self disposable.</summary>
+        public static IDisposable AddTo(this IDisposable disposable, Component gameObjectComponent)
+        {
+            if (gameObjectComponent == null)
+            {
+                disposable.Dispose();
+                return disposable;
+            }
+
+            return AddTo(disposable, gameObjectComponent.gameObject);
+        }
+
         /// <summary>
         /// <para>Add disposable(self) to CompositeDisposable(or other ICollection) and Dispose self on target gameObject has been destroyed.</para>
         /// <para>Return value is self disposable.</para>
@@ -27,6 +45,15 @@ namespace UniRx
         public static IDisposable AddTo(this IDisposable disposable, ICollection<IDisposable> container, GameObject gameObject)
         {
             return disposable.AddTo(container).AddTo(gameObject);
+        }
+
+        /// <summary>
+        /// <para>Add disposable(self) to CompositeDisposable(or other ICollection) and Dispose self on target gameObject has been destroyed.</para>
+        /// <para>Return value is self disposable.</para>
+        /// </summary>
+        public static IDisposable AddTo(this IDisposable disposable, ICollection<IDisposable> container, Component gameObjectComponent)
+        {
+            return disposable.AddTo(container).AddTo(gameObjectComponent);
         }
     }
 }
