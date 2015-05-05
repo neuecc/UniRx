d1b048c2cccd115105b7bd2406502da1ebabc8ea
Shogo Iwano <shiwano@gmail.com>
2015-05-05 10:20:21 +0900

    Make DisposableExtensions.AddTo method-chainable

--- Assets/UniRx/Scripts/Disposables/DisposableExtensions.cs
+++ Assets/UniRx/Scripts/Disposables/DisposableExtensions.cs
@@ -6,7 +6,8 @@ namespace UniRx
     public static partial class DisposableExtensions
     {
         /// <summary>Add disposable(self) to CompositeDisposable(or other ICollection). Return value is self disposable.</summary>
-        public static IDisposable AddTo(this IDisposable disposable, ICollection<IDisposable> container)
+        public static T AddTo<T>(this T disposable, ICollection<IDisposable> container)
+            where T : IDisposable
         {
             if (disposable == null) throw new ArgumentNullException("disposable");
             if (container == null) throw new ArgumentNullException("container");
