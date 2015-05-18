b7212f4a354fc6afa6d4e21594e16def2ecd95c3
neuecc <ils@neue.cc>
2015-05-18 14:54:15 +0900

    Fix nested ReactiveProperty can't show on Inspector

--- Assets/ObjectTest/Sandbox2.cs
+++ Assets/ObjectTest/Sandbox2.cs
@@ -3,16 +3,46 @@ using UniRx;
 using UniRx.Triggers;
 using System.Collections;
 using System.Linq;
+using System;
 
 public class Sandbox2 : MonoBehaviour
 {
+    [SerializeField]
+    public MySerializable MS;
+
+    [SerializeField]
+    public IntReactiveProperty Direct;
+
+
     void Start()
     {
-        var mt = this.gameObject.AddComponent<ObservableMouseTrigger>();
-        mt.OnMouseDownAsObservable()
-            .Subscribe(_ => Debug.Log("mouse down"));
+        Direct.Subscribe(x => Debug.Log("Direct " + x));
+        MS.IRP.Subscribe(x => Debug.Log("IRP " + x));
+        MS.Depth.IRP2.Subscribe(x => Debug.Log("IRP2 " + x));
     }
+}
 
 
 
+[Serializable]
+public class MySerializable
+{
+    [SerializeField]
+    public int Huga;
+
+    [SerializeField]
+    public IntReactiveProperty IRP;
+
+    [SerializeField]
+    public Depth2 Depth;
+}
+
+[Serializable]
+public class Depth2
+{
+    [SerializeField]
+    public int Hage;
+
+    [SerializeField]
+    public IntReactiveProperty IRP2;
 }
