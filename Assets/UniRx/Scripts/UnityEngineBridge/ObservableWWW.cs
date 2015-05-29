4db9084c834f440c3589afdc6f9a71a3209e2fce
Deniz Piri <denizpiri@gmail.com>
2015-05-29 14:07:10 +0200

    Fix WWWErrorException StatusCode for iOS IL2CPP.
    This fixes the parsing of RawErrorMessage for iOS IL2CPP.
    
    For some reason, on iPad Air with IL2CPP it is given with a column after
    the status code. That breaks the parsing of StatusCode field. However on
    iPhone 6, problem doesn't seem to occur.

--- Assets/UniRx/Scripts/UnityEngineBridge/ObservableWWW.cs
+++ Assets/UniRx/Scripts/UnityEngineBridge/ObservableWWW.cs
@@ -355,7 +355,7 @@ namespace UniRx
             this.ResponseHeaders = www.responseHeaders;
             this.HasResponse = false;
 
-            var splitted = RawErrorMessage.Split(' ');
+            var splitted = RawErrorMessage.Split(' ', ':');
             if (splitted.Length != 0)
             {
                 int statusCode;
