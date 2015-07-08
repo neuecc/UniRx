972c479cc68b52957fc45a8c0f01b5b5cc4c6d19
niwanaga <iwanaga@orange-cube.jp>
2015-07-08 18:27:22 +0900

    add build scripts

---
+++ build/Get-MSBuildPath.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.Synopsis
+   Visual Studio に付属するMSBuild.exeのパスを取得する関数。
+.EXAMPLE
+   Get-VsPath
+#>
+function Get-MSBuildPath
+{
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param()
+
+    end {
+        $Script:msbuild
+    }
+
+    begin {
+        if (Test-Path ${env:ProgramFiles(x86)})
+        {
+            $Script:ProgramFiles = ${env:ProgramFiles(x86)}
+        }
+        else
+        {
+            $Script:ProgramFiles = $env:ProgramFiles
+        }
+
+        if (Test-Path $($Script:ProgramFiles + '\MSBuild\14.0\Bin\MSBuild.exe'))
+        {
+            $Script:msbuild = $Script:ProgramFiles + '\MSBuild\14.0\Bin\MSBuild.exe'
+        }
+        else
+        {
+            $Script:msbuild = $Script:ProgramFiles + '\MSBuild\12.0\Bin\MSBuild.exe'
+        }
+    }
+}
---
+++ build/build.bat
@@ -0,0 +1,1 @@
+powershell.exe .\build.ps1
