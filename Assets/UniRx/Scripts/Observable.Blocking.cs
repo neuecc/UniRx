365acd653bddc5a554ff5bebcf937e1763ad7d22
Jochen Grün <jochen.gruen@deutschebahn.com>
2015-09-04 14:31:25 +0200

    Reverted file.

--- Assets/UniRx/Scripts/Observable.Blocking.cs
+++ Assets/UniRx/Scripts/Observable.Blocking.cs
@@ -27,16 +27,9 @@ namespace UniRx
             var ex = default(Exception);
 
             using (source.Subscribe(
-                onNext: x =>
-                    {
-                        seenValue = true; value = x;
-                    },
-                onError: x =>
-                    {
-                        ex = x; semaphore.Set();
-                    },
-                onCompleted: () => 
-                    semaphore.Set()))
+                onNext: x => { seenValue = true; value = x; },
+                onError: x => { ex = x; semaphore.Set(); },
+                onCompleted: () => semaphore.Set()))
             {
                 var waitComplete = (timeout == InfiniteTimeSpan)
                     ? semaphore.WaitOne()
