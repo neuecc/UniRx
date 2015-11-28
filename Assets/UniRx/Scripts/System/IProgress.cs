281c192dbe41258126650200b5806017e8ceb17d
neuecc <ils@neue.cc>
2015-11-28 19:15:55 +0900

    Add Progress<T>

--- Assets/UniRx/Scripts/System/IProgress.cs
+++ Assets/UniRx/Scripts/System/IProgress.cs
@@ -2,11 +2,28 @@
 
 #if !NETFX_CORE
 
+using System;
+
 namespace UniRx
 {
     public interface IProgress<T>
     {
         void Report(T value);
+    }
+
+    public class Progress<T> : IProgress<T>
+    {
+        readonly Action<T> report;
+
+        public Progress(Action<T> report)
+        {
+            this.report = report;
+        }
+
+        public void Report(T value)
+        {
+            report(value);
+        }
     }
 }
 
