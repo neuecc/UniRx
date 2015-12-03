3c1fcf3201ede04ee72e81fbd0deaeab513ff165
neuecc <ils@neue.cc>
2015-12-03 23:55:43 +0900

    Amb #97

--- Tests/UniRx.Console/Playground.cs
+++ Tests/UniRx.Console/Playground.cs
@@ -12,22 +12,61 @@ namespace UniRx
         public void Run()
         {
 
-            var xs = Observable.Range(1, 100)
-                .ObserveOn(Scheduler.ThreadPool)
-                .Subscribe(x => Console.WriteLine(x));
+            //var s1 = new Subject<int>();
+            //var s2 = new Subject<int>();
+
+            //s1.Amb(s2).Subscribe(x => Console.WriteLine(x));
 
 
-            //xs.Dispose();
+            //s1.OnNext(1000);
+            //s1.OnNext(1001);
+            //s1.OnNext(1002);
+            //s1.OnNext(1003);
+            //s1.OnNext(1004);
+            //s2.OnNext(2001);
+            //s2.OnNext(2002);
+            //s2.OnNext(2003);
+            //s2.OnNext(2004);
 
-            Console.ReadLine();
+            var xs = Observable.Return(10).Delay(TimeSpan.FromSeconds(1)).Concat(Observable.Range(1, 3));
+
+            var xss = Observable.Return(10).Concat(Observable.Range(1, 3));
+            var a = xss.ToArray().Wait();
+            var b = xss.ToArray().Wait();
+            var c = xss.ToArray().Wait();
 
 
+            var ys = Observable.Return(30).Delay(TimeSpan.FromSeconds(2)).Concat(Observable.Range(5, 3));
+
+            // win left
+            var result = xs.Amb(ys).ToArray().Wait();
+
+            result[0].Is(10);
+            result[1].Is(1);
+            result[2].Is(2);
+            result[3].Is(3);
+
+            // win right
+            result = ys.Amb(xs).ToArray().Wait();
+
+            result[0].Is(10);
+            result[1].Is(1);
+            result[2].Is(2);
+            result[3].Is(3);
         }
 
         static void ShowStackTrace()
         {
             Console.WriteLine("----------------");
             Console.WriteLine(new StackTrace().ToString());
+        }
+    }
+
+    public static class OEx
+    {
+        public static void Is<T>(this T t, T t2)
+        {
+            Console.WriteLine("T:" + t + " T2:" + t2);
         }
     }
 }
