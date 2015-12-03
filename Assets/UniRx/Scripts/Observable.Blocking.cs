9224108f822135815e470f2d9bdecfe881afe45e
neuecc <ils@neue.cc>
2015-12-03 17:18:27 +0900

    Wait #97

--- Assets/UniRx/Scripts/Observable.Blocking.cs
+++ Assets/UniRx/Scripts/Observable.Blocking.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace UniRx
 {
@@ -8,43 +6,12 @@ namespace UniRx
     {
         public static T Wait<T>(this IObservable<T> source)
         {
-            return WaitCore(source, true, InfiniteTimeSpan);
+            return new UniRx.Operators.Wait<T>(source, InfiniteTimeSpan).Run();
         }
 
         public static T Wait<T>(this IObservable<T> source, TimeSpan timeout)
         {
-            return WaitCore(source, true, timeout);
-        }
-
-        static T WaitCore<T>(IObservable<T> source, bool throwOnEmpty, TimeSpan timeout)
-        {
-            if (source == null) throw new ArgumentNullException("source");
-
-            var semaphore = new System.Threading.ManualResetEvent(false);
-
-            var seenValue = false;
-            var value = default(T);
-            var ex = default(Exception);
-
-            using (source.Subscribe(
-                onNext: x => { seenValue = true; value = x; },
-                onError: x => { ex = x; semaphore.Set(); },
-                onCompleted: () => semaphore.Set()))
-            {
-                var waitComplete = (timeout == InfiniteTimeSpan)
-                    ? semaphore.WaitOne()
-                    : semaphore.WaitOne(timeout);
-
-                if (!waitComplete)
-                {
-                    throw new TimeoutException("OnCompleted not fired.");
-                }
-            }
-
-            if (ex != null) throw ex;
-            if (throwOnEmpty && !seenValue) throw new InvalidOperationException("No Elements.");
-
-            return value;
+            return new UniRx.Operators.Wait<T>(source, timeout).Run();
         }
     }
 }
---
+++ Assets/UniRx/Scripts/Operators/Wait.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace UniRx.Operators
+{
+    internal class Wait<T> : IObserver<T>
+    {
+        static readonly TimeSpan InfiniteTimeSpan = new TimeSpan(0, 0, 0, 0, -1); // from .NET 4.5
+
+        readonly IObservable<T> source;
+        readonly TimeSpan timeout;
+
+        System.Threading.ManualResetEvent semaphore;
+
+        bool seenValue = false;
+        T value = default(T);
+        Exception ex = default(Exception);
+
+        public Wait(IObservable<T> source, TimeSpan timeout)
+        {
+            this.source = source;
+            this.timeout = timeout;
+        }
+
+        public T Run()
+        {
+            semaphore = new System.Threading.ManualResetEvent(false);
+            using (source.Subscribe(this))
+            {
+                var waitComplete = (timeout == InfiniteTimeSpan)
+                    ? semaphore.WaitOne()
+                    : semaphore.WaitOne(timeout);
+
+                if (!waitComplete)
+                {
+                    throw new TimeoutException("OnCompleted not fired.");
+                }
+            }
+
+            if (ex != null) throw ex;
+            if (!seenValue) throw new InvalidOperationException("No Elements.");
+
+            return value;
+        }
+
+        public void OnNext(T value)
+        {
+            seenValue = true;
+            this.value = value;
+        }
+
+        public void OnError(Exception error)
+        {
+            this.ex = error;
+            semaphore.Set();
+        }
+
+        public void OnCompleted()
+        {
+            semaphore.Set();
+        }
+    }
+}
