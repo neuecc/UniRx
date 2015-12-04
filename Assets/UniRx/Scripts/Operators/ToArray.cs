c3941549845a380ec4d8931e475a2a8445c50a28
neuecc <ils@neue.cc>
2015-12-04 21:10:55 +0900

    WIP(ToArray, ToList)

---
+++ Assets/UniRx/Scripts/Operators/ToArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniRx.Operators
+{
+    internal class ToArrayObservable<TSource> : OperatorObservableBase<TSource[]>
+    {
+        readonly IObservable<TSource> source;
+
+        public ToArrayObservable(IObservable<TSource> source)
+            : base(source.IsRequiredSubscribeOnCurrentThread())
+        {
+            this.source = source;
+        }
+
+        protected override IDisposable SubscribeCore(IObserver<TSource[]> observer, IDisposable cancel)
+        {
+            return source.Subscribe(new ToArray(observer, cancel));
+        }
+
+        class ToArray : OperatorObserverBase<TSource, TSource[]>
+        {
+            readonly List<TSource> list = new List<TSource>();
+
+            public ToArray(IObserver<TSource[]> observer, IDisposable cancel)
+                : base(observer, cancel)
+            {
+            }
+
+            public override void OnNext(TSource value)
+            {
+                try
+                {
+                    list.Add(value); // sometimes cause error on multithread
+                }
+                catch (Exception ex)
+                {
+                    OnError(ex);
+                    return;
+                }
+            }
+
+            public override void OnCompleted()
+            {
+                TSource[] result;
+                try
+                {
+                    result = list.ToArray();
+                }
+                catch (Exception ex) 
+                {
+                    OnError(ex);
+                    return;
+                }
+
+                base.observer.OnNext(result);
+                base.OnCompleted();
+            }
+        }
+    }
+}
---
+++ Assets/UniRx/Scripts/Operators/ToList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniRx.Operators
+{
+    internal class ToListObservable<TSource> : OperatorObservableBase<IList<TSource>>
+    {
+        readonly IObservable<TSource> source;
+
+        public ToListObservable(IObservable<TSource> source)
+            : base(source.IsRequiredSubscribeOnCurrentThread())
+        {
+            this.source = source;
+        }
+
+        protected override IDisposable SubscribeCore(IObserver<IList<TSource>> observer, IDisposable cancel)
+        {
+            return source.Subscribe(new ToList(observer, cancel));
+        }
+
+        class ToList : OperatorObserverBase<TSource, IList<TSource>>
+        {
+            readonly List<TSource> list = new List<TSource>();
+
+            public ToList(IObserver<IList<TSource>> observer, IDisposable cancel)
+                : base(observer, cancel)
+            {
+            }
+
+            public override void OnNext(TSource value)
+            {
+                try
+                {
+                    list.Add(value); // sometimes cause error on multithread
+                }
+                catch (Exception ex)
+                {
+                    OnError(ex);
+                    return;
+                }
+            }
+
+            public override void OnCompleted()
+            {
+                base.observer.OnNext(list);
+                base.OnCompleted();
+            }
+        }
+    }
+}
