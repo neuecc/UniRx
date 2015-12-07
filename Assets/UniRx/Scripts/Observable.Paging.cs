ff3713cf6269177c9fd9a66dffe4a7ab14ff8fdd
neuecc <ils@neue.cc>
2015-12-08 00:04:34 +0900

    Buffer #97

--- Assets/UniRx/Scripts/Observable.Paging.cs
+++ Assets/UniRx/Scripts/Observable.Paging.cs
@@ -124,27 +124,7 @@ namespace UniRx
             if (source == null) throw new ArgumentNullException("source");
             if (count <= 0) throw new ArgumentOutOfRangeException("count <= 0");
 
-            return Observable.Create<IList<T>>(observer =>
-            {
-                var list = new List<T>();
-
-                return source.Subscribe(x =>
-                {
-                    list.Add(x);
-                    if (list.Count == count)
-                    {
-                        observer.OnNext(list);
-                        list = new List<T>();
-                    }
-                }, observer.OnError, () =>
-                {
-                    if (list.Count > 0)
-                    {
-                        observer.OnNext(list);
-                    }
-                    observer.OnCompleted();
-                });
-            });
+            return new BufferObservable<T>(source, count, 0);
         }
 
         public static IObservable<IList<T>> Buffer<T>(this IObservable<T> source, int count, int skip)
@@ -153,43 +133,7 @@ namespace UniRx
             if (count <= 0) throw new ArgumentOutOfRangeException("count <= 0");
             if (skip <= 0) throw new ArgumentOutOfRangeException("skip <= 0");
 
-            return Observable.Create<IList<T>>(observer =>
-            {
-                var q = new Queue<List<T>>();
-
-                var index = -1;
-                return source.Subscribe(x =>
-                {
-                    index++;
-
-                    if (index % skip == 0)
-                    {
-                        q.Enqueue(new List<T>(count));
-                    }
-
-                    var len = q.Count;
-                    for (int i = 0; i < len; i++)
-                    {
-                        var list = q.Dequeue();
-                        list.Add(x);
-                        if (list.Count == count)
-                        {
-                            observer.OnNext(list);
-                        }
-                        else
-                        {
-                            q.Enqueue(list);
-                        }
-                    }
-                }, observer.OnError, () =>
-                {
-                    foreach (var list in q)
-                    {
-                        observer.OnNext(list);
-                    }
-                    observer.OnCompleted();
-                });
-            });
+            return new BufferObservable<T>(source, count, skip);
         }
 
         public static IObservable<IList<T>> Buffer<T>(this IObservable<T> source, TimeSpan timeSpan)
@@ -201,43 +145,7 @@ namespace UniRx
         {
             if (source == null) throw new ArgumentNullException("source");
 
-            return Observable.Create<IList<T>>(observer =>
-            {
-                var list = new List<T>();
-                var gate = new object();
-
-                var d = new CompositeDisposable(2);
-
-                // timer
-                d.Add(scheduler.Schedule(timeSpan, self =>
-                {
-                    List<T> currentList;
-                    lock (gate)
-                    {
-                        currentList = list;
-                        list = new List<T>();
-                    }
-
-                    observer.OnNext(currentList);
-                    self(timeSpan);
-                }));
-
-                // subscription
-                d.Add(source.Subscribe(x =>
-                {
-                    lock (gate)
-                    {
-                        list.Add(x);
-                    }
-                }, observer.OnError, () =>
-                {
-                    var currentList = list;
-                    observer.OnNext(currentList);
-                    observer.OnCompleted();
-                }));
-
-                return d;
-            });
+            return new BufferObservable<T>(source, timeSpan, timeSpan, scheduler);
         }
 
         public static IObservable<IList<T>> Buffer<T>(this IObservable<T> source, TimeSpan timeSpan, int count)
@@ -250,250 +158,24 @@ namespace UniRx
             if (source == null) throw new ArgumentNullException("source");
             if (count <= 0) throw new ArgumentOutOfRangeException("count <= 0");
 
-            return Observable.Create<IList<T>>(observer =>
-            {
-                var list = new List<T>();
-                var gate = new object();
-                var timerId = 0L;
-
-                var d = new CompositeDisposable(2);
-                var timerD = new SerialDisposable();
-
-                // timer
-                d.Add(timerD);
-                Action createTimer = () =>
-                {
-                    var currentTimerId = timerId;
-                    var timerS = new SingleAssignmentDisposable();
-                    timerD.Disposable = timerS; // restart timer(dispose before)
-                    timerS.Disposable = scheduler.Schedule(timeSpan, self =>
-                    {
-                        List<T> currentList;
-                        lock (gate)
-                        {
-                            if (currentTimerId != timerId) return;
-
-                            currentList = list;
-                            if (currentList.Count != 0)
-                            {
-                                list = new List<T>();
-                            }
-                        }
-                        if (currentList.Count != 0)
-                        {
-                            observer.OnNext(currentList);
-                        }
-                        self(timeSpan);
-                    });
-                };
-
-                createTimer();
-
-                // subscription
-                d.Add(source.Subscribe(x =>
-                {
-                    List<T> currentList = null;
-                    lock (gate)
-                    {
-                        list.Add(x);
-                        if (list.Count == count)
-                        {
-                            currentList = list;
-                            list = new List<T>();
-                            timerId++;
-                            createTimer();
-                        }
-                    }
-                    if (currentList != null)
-                    {
-                        observer.OnNext(currentList);
-                    }
-                }, observer.OnError, () =>
-                {
-                    lock (gate)
-                    {
-                        timerId++;
-                    }
-                    var currentList = list;
-                    observer.OnNext(currentList);
-                    observer.OnCompleted();
-                }));
-
-                return d;
-            });
+            return new BufferObservable<T>(source, timeSpan, count, scheduler);
         }
 
         public static IObservable<IList<T>> Buffer<T>(this IObservable<T> source, TimeSpan timeSpan, TimeSpan timeShift)
         {
-            return Buffer(source, timeSpan, timeShift, Scheduler.DefaultSchedulers.TimeBasedOperations);
+            return new BufferObservable<T>(source, timeSpan, timeShift, Scheduler.DefaultSchedulers.TimeBasedOperations);
         }
 
         public static IObservable<IList<T>> Buffer<T>(this IObservable<T> source, TimeSpan timeSpan, TimeSpan timeShift, IScheduler scheduler)
         {
             if (source == null) throw new ArgumentNullException("source");
 
-            return Observable.Create<IList<T>>(observer =>
-            {
-                var totalTime = TimeSpan.Zero;
-                var nextShift = timeShift;
-                var nextSpan = timeSpan;
-
-                var gate = new object();
-                var q = new Queue<IList<T>>();
-
-                var timerD = new SerialDisposable();
-
-                var createTimer = default(Action);
-                createTimer = () =>
-                {
-                    var m = new SingleAssignmentDisposable();
-                    timerD.Disposable = m;
-
-                    var isSpan = false;
-                    var isShift = false;
-                    if (nextSpan == nextShift)
-                    {
-                        isSpan = true;
-                        isShift = true;
-                    }
-                    else if (nextSpan < nextShift)
-                        isSpan = true;
-                    else
-                        isShift = true;
-
-                    var newTotalTime = isSpan ? nextSpan : nextShift;
-                    var ts = newTotalTime - totalTime;
-                    totalTime = newTotalTime;
-
-                    if (isSpan)
-                        nextSpan += timeShift;
-                    if (isShift)
-                        nextShift += timeShift;
-
-                    m.Disposable = scheduler.Schedule(ts, () =>
-                    {
-                        lock (gate)
-                        {
-                            if (isShift)
-                            {
-                                var s = new List<T>();
-                                q.Enqueue(s);
-                            }
-                            if (isSpan)
-                            {
-                                var s = q.Dequeue();
-                                observer.OnNext(s);
-                            }
-                        }
-
-                        createTimer();
-                    });
-                };
-
-                q.Enqueue(new List<T>());
-
-                createTimer();
-
-                return source.Subscribe(
-                    x =>
-                    {
-                        lock (gate)
-                        {
-                            foreach (var s in q)
-                                s.Add(x);
-                        }
-                    },
-                    observer.OnError,
-                    () =>
-                    {
-                        lock (gate)
-                        {
-                            foreach (var list in q)
-                            {
-                                observer.OnNext(list);
-                            }
-
-                            observer.OnCompleted();
-                        }
-                    }
-                );
-            });
+            return new BufferObservable<T>(source, timeSpan, timeShift, scheduler);
         }
 
         public static IObservable<IList<TSource>> Buffer<TSource, TWindowBoundary>(this IObservable<TSource> source, IObservable<TWindowBoundary> windowBoundaries)
         {
-            return Observable.Create<IList<TSource>>(observer =>
-            {
-                var list = new List<TSource>();
-                var gate = new object();
-
-                var d = new CompositeDisposable(2);
-
-                d.Add(source.Subscribe(Observer.Create<TSource>(
-                    x =>
-                    {
-                        lock (gate)
-                        {
-                            list.Add(x);
-                        }
-                    },
-                    ex =>
-                    {
-                        lock (gate)
-                        {
-                            observer.OnError(ex);
-                        }
-                    },
-                    () =>
-                    {
-                        lock (gate)
-                        {
-                            var currentList = list;
-                            list = new List<TSource>(); // safe
-                            observer.OnNext(currentList);
-                            observer.OnCompleted();
-                        }
-                    }
-                )));
-
-                d.Add(windowBoundaries.Subscribe(Observer.Create<TWindowBoundary>(
-                    w =>
-                    {
-                        List<TSource> currentList;
-                        lock (gate)
-                        {
-                            currentList = list;
-                            if (currentList.Count != 0)
-                            {
-                                list = new List<TSource>();
-                            }
-                        }
-                        if (currentList.Count != 0)
-                        {
-                            observer.OnNext(currentList);
-                        }
-                    },
-                    ex =>
-                    {
-                        lock (gate)
-                        {
-                            observer.OnError(ex);
-                        }
-                    },
-                    () =>
-                    {
-                        lock (gate)
-                        {
-                            var currentList = list;
-                            list = new List<TSource>(); // safe
-                            observer.OnNext(currentList);
-                            observer.OnCompleted();
-                        }
-                    }
-                )));
-
-                return d;
-            });
+            return new BufferObservable<TSource, TWindowBoundary>(source, windowBoundaries);
         }
 
         /// <summary>Projects old and new element of a sequence into a new form.</summary>
