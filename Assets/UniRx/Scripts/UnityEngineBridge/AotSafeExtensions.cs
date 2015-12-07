3a9750caa1c5c7b3abe1b5719ded97cd4bfc1b14
neuecc <ils@neue.cc>
2015-12-07 20:52:54 +0900

    BreakingChange: Remove WrapValueToClass

--- Assets/UniRx/Scripts/UnityEngineBridge/AotSafeExtensions.cs
+++ Assets/UniRx/Scripts/UnityEngineBridge/AotSafeExtensions.cs
@@ -17,33 +17,5 @@ namespace UniRx
                 }
             }
         }
-
-        public static IObservable<Tuple<T>> WrapValueToClass<T>(this IObservable<T> source)
-            where T : struct
-        {
-            var dummy = 0;
-            return Observable.Create<Tuple<T>>(observer =>
-            {
-                return source.Subscribe(Observer.Create<T>(x =>
-                {
-                    dummy.GetHashCode(); // capture outer value
-                    var v = new Tuple<T>(x);
-                    observer.OnNext(v);
-                }, observer.OnError, observer.OnCompleted));
-            });
-        }
-
-        public static IEnumerable<Tuple<T>> WrapValueToClass<T>(this IEnumerable<T> source)
-            where T : struct
-        {
-            var e = ((IEnumerable)source).GetEnumerator();
-            using (e as IDisposable)
-            {
-                while (e.MoveNext())
-                {
-                    yield return new Tuple<T>((T)e.Current);
-                }
-            }
-        }
     }
 }
