384760da35ef535d40683c9bf68916f86207f378
neuecc <ils@neue.cc>
2015-12-14 22:53:03 +0900

    commit

---
+++ Assets/ObjectTest/CountOnly.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountOnly : MonoBehaviour
+{
+    public Text text;
+
+    int count = 0;
+
+    void Update()
+    {
+        text.text = (count++).ToString();
+    }
+}
