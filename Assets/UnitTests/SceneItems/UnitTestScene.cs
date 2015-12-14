6c24cae5fd200ff910c9685bcfc87f4b35367441
neuecc <ils@neue.cc>
2015-12-14 23:23:34 +0900

    Working CHeck

---
+++ Assets/UnitTests/SceneItems/UnitTestScene.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnityEngine.UI;
+using System;
+
+namespace UniRx.Tests
+{
+    public class UnitTestScene : MonoBehaviour
+    {
+        public Button buttonPrefab;
+        public GameObject buttonVertical;
+
+        public Result resultPrefab;
+        public GameObject resultVertical;
+
+        public Text text;
+
+        public Button sample;
+
+        int counter;
+
+        void Start()
+        {
+            try
+            {
+                text.text = "sample...";
+
+                //sample.OnClickAsObservable().Subscribe(_ =>
+                //{
+                //    text.text = "start...";
+                //    MainThreadDispatcher.StartCoroutine(ObservableConcurrencyTest.Run(resultPrefab, resultVertical));
+                //});
+
+                //// UnitTest uses Wait, it can't run on MainThreadScheduler.
+                //Scheduler.DefaultSchedulers.SetDotNetCompatible();
+                //MainThreadDispatcher.Initialize();
+
+                //UnitTests.SetButtons(buttonPrefab, buttonVertical, resultPrefab, resultVertical);
+            }
+            catch (Exception ex)
+            {
+                text.text = ex.ToString();
+            }
+        }
+
+        public void OnGUI()
+        {
+            if (GUI.Button(new Rect(0, 0, 100, 100), (counter++).ToString()))
+            {
+
+            }
+        }
+
+        public void Update()
+        {
+            text.text = (counter++).ToString();
+        }
+    }
+}
---
+++ Assets/UnitTests/Tools/TestUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniRx.Tests
+{
+    public static class TestUtil
+    {
+        public static T[] ToArrayWait<T>(this IObservable<T> source)
+        {
+            return source.ToArray().Wait();
+        }
+    }
+}
