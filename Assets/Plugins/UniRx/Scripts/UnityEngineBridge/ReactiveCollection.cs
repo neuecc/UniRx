1e56dd84d4360ee7661bc71facd38c4170da4455
neuecc <ils@neue.cc>
2016-05-07 20:13:22 +0900

    Fix ambigious property of IReactiveCollection.Count, Indexer. close #126

--- Assets/Plugins/UniRx/Scripts/UnityEngineBridge/ReactiveCollection.cs
+++ Assets/Plugins/UniRx/Scripts/UnityEngineBridge/ReactiveCollection.cs
@@ -137,6 +137,8 @@ namespace UniRx
 
     public interface IReactiveCollection<T> : IList<T>, IReadOnlyReactiveCollection<T>
     {
+        new int Count { get; }
+        new T this[int index] { get; set; }
         void Move(int oldIndex, int newIndex);
     }
 
