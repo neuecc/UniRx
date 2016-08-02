983f3f211f2fef18ca90979cd04bf8fcbb1600b9
neuecc <ils@neue.cc>
2016-08-02 11:58:38 +0900

    UniRx.Library package

--- Dlls/UniRx.Library/Properties/AssemblyInfo.cs
+++ Dlls/UniRx.Library/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.4.0.0")]
-[assembly: AssemblyFileVersion("5.4.0.0")]
+[assembly: AssemblyVersion("5.4.0.1")]
+[assembly: AssemblyFileVersion("5.4.0.1")]
 
 [assembly: InternalsVisibleTo("UniRx.Unity")]
