833ce9166c0822ac576612576b5703f3a024673a
Mathieu Frenette <mfrenette@lvlstudio.com>
2017-05-11 13:44:21 -0400

    Adapted Observable.Using() from https://github.com/Reactive-Extensions/Rx.NET and created unit tests.

---
+++ Assets/Plugins/UniRx/Scripts/Observable.Using.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UniRx
+{
+    public static partial class Observable
+    {
+        // Adapted from: https://github.com/Reactive-Extensions/Rx.NET/blob/371c83c621562a2259580a03f0cb5bf8680ea720/Rx.NET/Source/System.Reactive.Linq/Reactive/Linq/QueryLanguage.Creation.cs#L417-L441
+        public static IObservable<TSource> Using<TSource, TResource>(
+            Func<TResource> resourceFactory,
+            Func<TResource, IObservable<TSource>> observableFactory)
+            where TResource : IDisposable
+        {
+            return Create<TSource>(observer =>
+            {
+                IObservable<TSource> source;
+                var disposable = Disposable.Empty;
+                try
+                {
+                    var resource = resourceFactory();
+                    if (resource != null)
+                        disposable = resource;
+
+                    source = observableFactory(resource);
+                }
+                catch (Exception exception)
+                {
+                    return new CompositeDisposable(
+                        Throw<TSource>(exception).Subscribe(observer),
+                        disposable);
+                }
+
+                return new CompositeDisposable(
+                    source.Subscribe(observer),
+                    disposable);
+            });
+        }
+    }
+}
---
+++ Tests/UniRx.Tests/Observable.UsingTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UniRx.Tests
+{
+    [TestClass]
+    public class ObservableUsingTest
+    {
+        private BooleanDisposable _resource;
+        private Subject<Unit> _subject;
+        private IObservable<Unit> _observable;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _resource = null;
+            _subject = new Subject<Unit>();
+            _observable = Observable.Using(
+                () => new BooleanDisposable(),
+                res =>
+                {
+                    _resource = res;
+                    return _subject;
+                });
+        }
+
+        [TestMethod]
+        public void TestSubscribingAllocatesResourceAndPassesItToObservableFactory()
+        {
+            _resource.IsNull();
+            _observable.Subscribe();
+            _resource.IsNotNull();
+        }
+
+        [TestMethod]
+        public void TestDisposingSubscriptionAlsoDisposesResource()
+        {
+            var subscription = _observable.Subscribe();
+            _resource.IsDisposed.IsFalse();
+            subscription.Dispose();
+            _resource.IsDisposed.IsTrue();
+        }
+
+        [TestMethod]
+        public void TestCompletingObservableDisposesResource()
+        {
+            _observable.Subscribe();
+            _resource.IsDisposed.IsFalse();
+            _subject.OnCompleted();
+            _resource.IsDisposed.IsTrue();
+        }
+
+        [TestMethod]
+        public void TestThrowingExceptionWithinResourceFactoryEmitsErrorViaObservable()
+        {
+            _observable = Observable.Using<Unit, BooleanDisposable>(
+                () => throw new InvalidOperationException(),
+                res => Observable.ReturnUnit());
+
+            Exception exception = null;
+            _observable.Subscribe(null, ex => exception = ex);
+
+            exception.IsInstanceOf<InvalidOperationException>();
+        }
+    }
+}
