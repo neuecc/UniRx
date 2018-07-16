2ed7a3658626f395421b74abce08cc1d079f6b5c
Yoshifumi Kawai <ils@neue.cc>
2018-07-16 21:45:24 +0900

    Add UniTask.SwitchToThreadPool/SwitchToTaskPool

---
+++ Assets/Plugins/UniRx/Scripts/Async/UniTask.Threading.cs
@@ -0,0 +1,37 @@
+﻿#if CSHARP_7_OR_LATER
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UniRx.Async
+{
+    public partial struct UniTask
+    {
+        static readonly WaitCallback switchToThreadPoolCallback = CallbackPromiseSetResult;
+        static readonly Action<object> switchToTaskPoolCallback = CallbackPromiseSetResult;
+
+        public static UniTask SwitchToThreadPool()
+        {
+            var promise = new Promise<AsyncUnit>();
+            ThreadPool.UnsafeQueueUserWorkItem(switchToThreadPoolCallback, promise);
+            return new UniTask(promise);
+        }
+
+        public static UniTask SwitchToTaskPool(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var promise = new Promise<AsyncUnit>();
+            Task.Factory.StartNew(switchToTaskPoolCallback, promise, cancellationToken, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
+            return new UniTask(promise);
+        }
+
+        static void CallbackPromiseSetResult(object state)
+        {
+            var promise = (Promise<AsyncUnit>)state;
+            promise.SetResult(AsyncUnit.Default);
+        }
+    }
+}
+
+#endif
