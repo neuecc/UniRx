18c9befcd0f4049e845923e4ec66fc211a23adc6
KAW0 <16373327+KAW0@users.noreply.github.com>
2018-11-10 20:39:07 +0100

    Add DynamicSubject.
    DynamicSubject is similar to BehaviorSubject but without defaut value.

---
+++ Assets/Plugins/UniRx/Scripts/Subjects/DynamicSubject.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using UniRx.InternalUtil;
+
+namespace UniRx
+{
+    public sealed class DynamicSubject<T> : ISubject<T>, IDisposable, IOptimizedObservable<T>
+    {
+        object observerLock = new object();
+        bool isStopped;
+        bool isDisposed;
+        Exception lastError;
+        IObserver<T> outObserver = EmptyObserver<T>.Instance;
+
+        public DynamicSubject()
+        {
+        }
+
+
+        public bool HasObservers
+        {
+            get
+            {
+                return !(outObserver is EmptyObserver<T>) && !isStopped && !isDisposed;
+            }
+        }
+
+        public void OnCompleted()
+        {
+            IObserver<T> old;
+            lock (observerLock)
+            {
+                ThrowIfDisposed();
+                if (isStopped) return;
+
+                old = outObserver;
+                outObserver = EmptyObserver<T>.Instance;
+                isStopped = true;
+            }
+
+            old.OnCompleted();
+        }
+
+        public void OnError(Exception error)
+        {
+            if (error == null) throw new ArgumentNullException("error");
+
+            IObserver<T> old;
+            lock (observerLock)
+            {
+                ThrowIfDisposed();
+                if (isStopped) return;
+
+                old = outObserver;
+                outObserver = EmptyObserver<T>.Instance;
+                isStopped = true;
+                lastError = error;
+            }
+
+            old.OnError(error);
+        }
+
+        public void OnNext(T value)
+        {
+            outObserver.OnNext(value);
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            if (observer == null) throw new ArgumentNullException("observer");
+
+            var ex = default(Exception);
+            var subscription = default(Subscription);
+
+            lock (observerLock)
+            {
+                ThrowIfDisposed();
+                if (!isStopped)
+                {
+                    var listObserver = outObserver as ListObserver<T>;
+                    if (listObserver != null)
+                    {
+                        outObserver = listObserver.Add(observer);
+                    }
+                    else
+                    {
+                        var current = outObserver;
+                        if (current is EmptyObserver<T>)
+                        {
+                            outObserver = observer;
+                        }
+                        else
+                        {
+                            outObserver = new ListObserver<T>(new ImmutableList<IObserver<T>>(new[] { current, observer }));
+                        }
+                    }
+                    subscription = new Subscription(this, observer);
+                }
+                else
+                {
+                    ex = lastError;
+                }
+            }
+
+            if (subscription != null)
+            {
+                return subscription;
+            }
+            else if (ex != null)
+            {
+                observer.OnError(ex);
+            }
+            else
+            {
+                observer.OnCompleted();
+            }
+
+            return Disposable.Empty;
+        }
+
+        public void Dispose()
+        {
+            lock (observerLock)
+            {
+                isDisposed = true;
+                outObserver = DisposedObserver<T>.Instance;
+                lastError = null;
+            }
+        }
+
+        void ThrowIfDisposed()
+        {
+            if (isDisposed) throw new ObjectDisposedException("");
+        }
+
+        public bool IsRequiredSubscribeOnCurrentThread()
+        {
+            return false;
+        }
+
+        class Subscription : IDisposable
+        {
+            readonly object gate = new object();
+            DynamicSubject<T> parent;
+            IObserver<T> unsubscribeTarget;
+
+            public Subscription(DynamicSubject<T> parent, IObserver<T> unsubscribeTarget)
+            {
+                this.parent = parent;
+                this.unsubscribeTarget = unsubscribeTarget;
+            }
+
+            public void Dispose()
+            {
+                lock (gate)
+                {
+                    if (parent != null)
+                    {
+                        lock (parent.observerLock)
+                        {
+                            var listObserver = parent.outObserver as ListObserver<T>;
+                            if (listObserver != null)
+                            {
+                                parent.outObserver = listObserver.Remove(unsubscribeTarget);
+                            }
+                            else
+                            {
+                                parent.outObserver = EmptyObserver<T>.Instance;
+                            }
+
+                            unsubscribeTarget = null;
+                            parent = null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
